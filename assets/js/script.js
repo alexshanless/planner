b79f30c50372069280facb732f996bff8c2d3d4d
Alex Bilba <alex.shanless@gmail.com>
2020-01-19 20:50:11 -0800

    updating local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,81 @@
-var now = moment()
+var schedule = JSON.parse(localStorage.getItem('dayplanner'))
+// var schedule = localStorage.getItem('dayplanner')
+console.log(schedule)
+if (!schedule) {
+    schedule = {}
+}
 
-setInterval(function(){    
+console.log(schedule['9 am'])
+
+setInterval(function () {
     $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, hh:mm a"))
 }, 1 * 1000)
 
-for(var i = 8; i <= 17; i++){
-    CreateRow(i, "")
+for (var i = 8; i <= 17; i++) {
+    CreateRow(i)
 }
 
-function CreateRow(time, event){
-    console.log(time)
+function CreateRow(time) {
     var thisMoment = moment(time, "H")
+    var key = thisMoment.format('h a')
+
+    // schedule['9 am']
+    // schedule['10 am']
+    // schedule['11 am']
+    var event = schedule[key]
 
     var row = $("<div>").addClass('row time-block')
-    var timeBlock = $("<div>").addClass("col-1 hour").text(thisMoment.format('h a'))
+    var timeBlock = $("<div>").addClass("col-1 hour").text(key)
     var textarea = $("<textarea>").addClass('description col-10').val(event)
     var saveBtn = $("<button>").addClass('saveBtn col-1').text('Save')
 
     var isSameHour = thisMoment.isSame(moment(), 'hour')
     var isBefore = thisMoment.isBefore(moment(), 'hour')
     var isAfter = thisMoment.isAfter(moment(), 'hour')
-    
-    if(isSameHour){
-        console.log("SAME HOUR")
+
+    if (isSameHour) {
         textarea.addClass('present')
     }
-    else if(isBefore){
-        console.log("IS BEFORE")
+    else if (isBefore) {
         textarea.addClass('past')
     }
-    else if(isAfter){
-        console.log("IS AFTER")
+    else if (isAfter) {
         textarea.addClass('future')
     }
-    
+
     row.append(timeBlock, textarea, saveBtn)
     $("#timeSlots").append(row)
-    
- 
-    
 }
 
-var saveBtn = $('.saveBtn');
-saveBtn.on('click', function(){
+
+$('.saveBtn').on('click', function () {
     let textArea = $(this).parent().children('textarea').val();
-    console.log(textArea);
-    let timeSlot =  $(this).parent().children('div.col-1.hour').text()
-    console.log(timeSlot);
-    localStorage.setItem(timeSlot, textArea);
-    console.log(localStorage);
-    
+    let timeSlot = $(this).parent().children('div.col-1.hour').text()
+    //schedule['9 am'] = 'some event'
+    schedule[timeSlot] = textArea
+    console.log(schedule)
+
+    localStorage.setItem("dayplanner", JSON.stringify(schedule))
+    // localStorage.setItem("dayplanner", schedule)
 })
+
+// var data = {
+//     name: "Ben"
+// }
+
+// var key = 'name'
+
+// console.log(data.name)
+// console.log(data.key)
+// data.age = 31
+// console.log(data)
+
+// key = 'job'
+// data[key] = 'tutor'
+// data['gender'] = 'male'
+
+// console.log(data['name'])
+// console.log(data[key])
+// console.log(data)
+
+// localStorage.setItem("dayplanner", JSON.stringify({ data: 'someData' }))
